e295725583a364c7c5f86c4048029d9ed6cd1974
Axel Faure <afaure05@debian.univ-jfc.fr>
2019-02-13 11:45:09 +0100

    Ajout de methodes

--- src/Modele.java
+++ src/Modele.java
@@ -1,9 +1,10 @@
 import java.awt.Color;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Modele {
 	
-	Color[] COULEURS = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange, Color.white, Color.black};
+	static Color[] COULEURS = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange, Color.white, Color.black};
 	int N_TENTATIVES, DIFFICULTE;
 	enum Etat {EN_COURS, GAGNE, PERDU};
 	
@@ -27,8 +28,14 @@ public class Modele {
 			combinaison.rangee[k] = COULEURS[rand.nextInt(COULEURS.length)];
 		}
 	}
-	public static void main(String[] args) {
-		Modele mod = new Modele(5, 10);
-		System.out.println(mod.combinaison.rangee[0]);
+	
+	public void archiver(Rangee r){
+		this.propositions[this.tentative] = r;
+	}
+	
+	public Etat etatCourant(){
+		if(this.tentative < this.N_TENTATIVES) this.etat = Etat.EN_COURS;
+		if(this.tentative > this.N_TENTATIVES) this.etat = Etat.PERDU;
+		return this.etat;
 	}
 }
--- src/Rangee.java
+++ src/Rangee.java
@@ -4,13 +4,41 @@ public class Rangee {
 	
 	Color rangee[];
 	int taille, indiceJeton;
-	int[] resultat;
+	Color[] resultat;
 	
 	public Rangee(int diff){
+		
 		this.taille = diff;
 		this.indiceJeton = 0;
 		this.rangee = new Color[diff];
+		this.resultat = new Color[diff];
 	}
 	
+	public void equals(Rangee r){
+		
+		//Premier passage pour determiner les pions bien places
+		for(int i = 0; i<this.taille; i++){
+			if(this.rangee[i] == r.rangee[i]) this.resultat[i] = Color.black;
+		}
+		
+ 		//Deuxième passage pour determiner les pions presents mais mal places
+		for(int i = 0; i<this.taille; i++){
+			if(this.resultat[i] == Color.black) continue;
+			for(int j = 0; j<r.taille; j++){
+				if(this.rangee[i] == r.rangee[j] && !this.resultat[j].equals(null)){
+					this.resultat[i] = Color.white;
+				}
+			}
+		} 
+	}
 	
+	public boolean estCorrecte(){
+		
+		int k = 0;
+		while(this.resultat[k] == Color.black){
+			k++;
+		}
+		if(k == this.taille) return true;
+		return false;
+	}
 }

da76f4d547a2ce1f712d68914d066eef7b9cb462
Axel Faure <afaure05@debian.univ-jfc.fr>
2019-02-13 12:06:47 +0100

    Modification construteur Modele

--- src/Modele.java
+++ src/Modele.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class Modele {
 	
 	static Color[] COULEURS = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange, Color.white, Color.black};
-	int N_TENTATIVES, DIFFICULTE;
+	static int N_TENTATIVES = 5;
+	static int DIFFICULTE = 4;
 	enum Etat {EN_COURS, GAGNE, PERDU};
 	
 	Etat etat;
@@ -13,9 +14,19 @@ public class Modele {
 	Rangee propositions[];
 	int tentative;
 	
-	//public Modele() {}
+	public Modele() {
+		this.etat = Etat.EN_COURS;
+		this.tentative = 1;
+		this.combinaison = new Rangee(Modele.DIFFICULTE);
+
+		Random rand = new Random();
+
+		for(int k = 0; k<Modele.DIFFICULTE; k++){
+			combinaison.rangee[k] = COULEURS[rand.nextInt(COULEURS.length)];
+		}
+	}
 	
-	public Modele(int diff, int tentatives){
+	/*public Modele(int diff, int tentatives){
 		this.DIFFICULTE = diff;
 		this.N_TENTATIVES = tentatives;
 		this.etat = Etat.EN_COURS;
@@ -27,7 +38,7 @@ public class Modele {
 		for(int k = 0; k<diff; k++){
 			combinaison.rangee[k] = COULEURS[rand.nextInt(COULEURS.length)];
 		}
-	}
+	}*/
 	
 	public void archiver(Rangee r){
 		this.propositions[this.tentative] = r;
